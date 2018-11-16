ab443b14943a85b262a6121b781223a69ef6cef7
Jonathan Diego Rodríguez Rodríguez <jon_eldiablo@hotmail.com>
2018-11-16 13:48:19 -0600

    seed component

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-generator-node-component
+# generator-node-component
+
+Done
+
+- Babel and build lib
+
+TODO:
+
+- add testing
+- add option command line.
+- add option microservice.
+- add option integration (like travis, build automagically, etc).
+- add git init and repo
--- generators/app/index.js
+++ generators/app/index.js
@@ -2,6 +2,7 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const path = require('path');
 
 module.exports = class extends Generator {
   prompting() {
@@ -12,10 +13,24 @@ module.exports = class extends Generator {
 
     const prompts = [
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
+        name: 'hyphenName',
+        message: `Component ${chalk.bold.yellow('hyphen')} name`,
+        default: process
+          .cwd()
+          .split(path.sep)
+          .pop()
+      },
+      {
+        name: 'description',
+        message: 'Description: '
+      },
+      {
+        name: 'author',
+        message: 'Author: '
+      },
+      {
+        name: 'license',
+        message: 'License: '
       }
     ];
 
@@ -26,14 +41,56 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.fs.copyTpl(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt', 'demo.other'),
-      this.props
-    );
+    let capitalize = string => string.replace(/\b\w/g, l => l.toUpperCase());
+    let firstLower = string => string.charAt(0).toLowerCase() + string.slice(1);
+    let className = this.props.hyphenName
+      .split(/[ -_]/g)
+      .map(seg => capitalize(seg))
+      .join('');
+    let vars = {
+      hyphenName: this.props.hyphenName,
+      className: className,
+      objectName: firstLower(className),
+      description: this.props.description,
+      author: this.props.author,
+      license: this.props.license
+    };
+
+    let classFile = {
+      f: ['index'],
+      origin: 'src/{file}.ejs',
+      dest: `src/${className}.js`
+    };
+    let scriptFiles = {
+      f: ['main', 'bin'],
+      origin: '{file}.ejs',
+      dest: '{file}.js'
+    };
+    let slashFiles = {
+      f: ['package.json'],
+      origin: '_{file}',
+      dest: '{file}'
+    };
+    let dotFiles = {
+      f: ['babelrc'],
+      origin: '{file}',
+      dest: '.{file}'
+    };
+
+    let files = [classFile, scriptFiles, slashFiles, dotFiles];
+
+    files.forEach(element => {
+      element.f.forEach(file => {
+        let origin = element.origin.replace('{file}', file);
+        let dest = element.dest.replace('{file}', file);
+        this.fs.copyTpl(this.templatePath(origin), this.destinationPath(dest), vars);
+      });
+    });
   }
 
   install() {
-    this.installDependencies();
+    this.installDependencies({
+      bower: false
+    });
   }
 };
