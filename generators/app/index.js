bc2e62d0e22279b8376c351c8be0e1bd9b6a1ec4
Jonathan Diego Rodríguez Rodríguez <jon_eldiablo@hotmail.com>
2018-11-16 11:25:09 -0600

    init generator

---
+++ generators/app/index.js
@@ -0,0 +1,39 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+
+module.exports = class extends Generator {
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(
+      yosay(`Welcome to the superb ${chalk.red('generator-node-component')} generator!`)
+    );
+
+    const prompts = [
+      {
+        type: 'confirm',
+        name: 'someAnswer',
+        message: 'Would you like to enable this option?',
+        default: true
+      }
+    ];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  }
+
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath('dummyfile.txt'),
+      this.destinationPath('dummyfile.txt', 'demo.other'),
+      this.props
+    );
+  }
+
+  install() {
+    this.installDependencies();
+  }
+};
